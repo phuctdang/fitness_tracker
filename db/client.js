fa0e0cb9509976abce9953f473e1dd9c4507dabf
phuctdang <phuctrungdang@gmail.com>
2024-04-23 17:32:29 -0700

    updated

--- db/client.js
+++ db/client.js
@@ -1,5 +1,5 @@
 // This creates and define a client, but does NOT connect to the client
 const { Client } = require('pg');
-const client = new Client ('postgres://phuctdang:phDA501748!@localhost:5432/fitness_tracker')
+const client = new Client ('postgres://@localhost:5432/fitness_tracker')
 
 module.exports = client;
